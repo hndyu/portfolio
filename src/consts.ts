a84b7f79cda829b515c9e4c9ce770a9c9c1f1dec
hndyu <4225495+hndyu@users.noreply.github.com>
2025-04-20 13:52:24 +0900

    Initial settings

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'hndyu';
+export const SITE_DESCRIPTION = 'hndyuのポートフォリオ、学習・更新記録を公開するサイト。';
